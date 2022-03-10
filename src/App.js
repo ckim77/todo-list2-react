6617e6b3248af0550f7d6f004c1278cdb28b752d
Chris Kim <ckjr92@gmail.com>
2022-03-09 21:52:19 -0700

    needs delete function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from "react"
 import './App.css';
 
-function App() {
+export default function App() {
+  const [task, setTask] = useState('');
+  const [list, updateList] = useState([]);
+
+  function handleChange (evt) {
+    setTask(evt.target.value);
+  }
+
+  function handleSubmit() {
+    updateList(arr => [...arr, task]);
+  }
+
   return (
-    <div className="App">
-      <h1>Todo List</h1>
+    
+    <div className = "App">
+      <h1>To-do List </h1>
+      <input
+        placeholder="Add Task"
+        type = 'text'
+        value = {[task]}
+        onChange = {handleChange}
+      />
+
+      <input
+        value = "Add Task"
+        type = 'submit'
+        onClick = {handleSubmit}
+        />
+      
+      <div>
+        {list.map(e => <div>{ e }</div> )} 
+      </div>
+
     </div>
-  );
+
+    
+
+  )
 }
 
-export default App;
